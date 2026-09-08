154d54aebd40e982875623dd805ef364b3dc8f1c
Kronmillaku <millakukron13@gmail.com>
2026-09-08 19:20:09 +0200

    Create twodimensional.php

---
+++ twodimensional.php
@@ -0,0 +1,18 @@
+<?php 
+$dogs = array (
+    array("Husky","Siberia",20),
+    array("Bulldog","England",15),
+    array("Pitbull","USA",25)
+
+);
+
+echo $dogs[0][0] .": Origin " . $dogs[0][1] .", Life Span: " .$dogs[0][2]. ".<br>";
+
+
+
+
+
+
+
+
+?>
